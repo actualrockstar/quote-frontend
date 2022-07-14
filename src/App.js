5d6e44c88540b7e1b929b2a831e09fcbe9514193
Kay <mmj342@nyu.edu>
2022-07-14 15:40:58 -0400

    updates demo refactor

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 
 import './App.css';
 import axios from 'axios';
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import edgy from './edgy.png';
-import logo from './am-labs-logo.png'
+import logo from './amb-labs-logo.png'
 
 
 
 function App() {
   const [quote, setQuote] = useState({quote:[]});
-  const baseURL = "http://quote.default.svc.cluster.local:80/"
+  //For running React App locally
+  //const baseURL = "quote.default.svc.cluster.local:80/"
+  //For running in Docker container
+  const baseURL = "/backend/"
+  const config = {
+    headers: {
+      "x-telepresence-id": "20af3d89-4052-4d08-8fb7-0603fc9bd53c:quotefrontend",
+    }
+  }
+
+
 
   const getQuote = () => {
-    axios.get(baseURL)
+    axios.get(baseURL, config)
       .then((res, err) => {
         if(res.status === 200){
           console.log(res.data)
@@ -28,7 +38,7 @@ function App() {
       <header className="App-header">
         {/* Replace the image with the Ambassador Logo by 
         replacing "edgy" with "logo" below */}
-        <img src={edgy}/> 
+        <img src={logo} alt="image"/> 
         <div title={"Server: " + quote.server}>
         {quote.quote}
         </div>
